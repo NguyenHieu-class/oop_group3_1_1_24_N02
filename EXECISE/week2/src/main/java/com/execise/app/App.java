23ce112dbc6746426b9d3f96c5526a09500801fc
Hiếu Nguyễn <22010103@st.phenikaa-uni.edu.vn>
2024-09-11 13:28:14 +0700

    Update execises

---
+++ EXECISE/week2/src/main/java/com/execise/app/App.java
@@ -0,0 +1,35 @@
+package com.execise.app;
+import com.execise.app.*;
+
+public class App {
+    public static void main(String[] args) {
+
+        //Time.java
+        Time t1 = new Time();
+        Time t2 = new Time(2, 30, 45);
+        System.out.println(t1);
+        System.out.println(t2);
+
+        //Book.java
+        Book b1 = new Book();
+        Book b2 = new Book("Java Programming", "James Gosling", 45.5);
+        System.out.println(b1);
+        System.out.println(b2);
+
+        //NameNumber.java
+        NameNumber nn1 = new NameNumber();
+        NameNumber nn2 = new NameNumber("John", "Doe", "1234567890");
+        System.out.println(nn1);
+        System.out.println(nn2);
+
+        //NNCollection.java
+        NNCollection nnc = new NNCollection();
+        nnc.insert(nn1);
+        nnc.insert(nn2);
+        System.out.println(nnc.findNumber("Doe"));
+
+        //Recursion.java
+        Recursion r = new Recursion();
+        System.out.println(r.Recursion(5));
+    }
+}
---
+++ EXECISE/week2/src/main/java/com/execise/app/Recursion.java
@@ -0,0 +1,21 @@
+package com.execise.app;
+
+public class Recursion {
+    int num;
+
+    // Constructor
+    Recursion() {
+    } // default constructor- remember ; before }
+
+    Recursion(int number) {
+    } // constructor for int
+
+    public int Recursion(int number) {
+        this.num = number;
+        if (this.num <= 1) // base case
+            return 1;
+        else
+            return this.num * Recursion(this.num - 1);
+    }
+
+}
